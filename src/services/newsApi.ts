8cd47439e08f8f3be59d87c56b1d0a0f734d9823
bilalshahid929 <bilalshahid929@gmail.com>
2025-01-27 05:16:20 +0500

    fix: remove the keys

--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -6,13 +6,6 @@ const {
   VITE_APP_THE_GUARDIAN_API_KEY,
 } = import.meta.env;
 
-export const API_KEYS = {
-  NEWS_API: VITE_APP_NEWS_API_KEY || "4d7397ed03c74ac68ada2d73b7eca5ca",
-  NY_TIMES: VITE_APP_NEW_YORK_TIMES_KEY || "uvXeQ3yYDdhDsKcSfRWkUAtOHN6tIVYe",
-  THE_GUARDIAN:
-    VITE_APP_THE_GUARDIAN_API_KEY || "50887b5b-52a9-435f-b5e2-fd0a17568d18",
-};
-
 export const API_URLS = {
   NEWS_API: (country = "us", category = "", query = "") =>
     `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&q=${query}&apiKey=${API_KEYS.NEWS_API}`,
--- src/store/slices/newsSlice.ts
+++ src/store/slices/newsSlice.ts
@@ -3,28 +3,13 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { fetchData, API_URLS } from "@/services/newsApi";
 
 // Types
-export enum Language {
-  ar = "ar",
-  de = "de",
-  en = "en",
-  es = "es",
-  fr = "fr",
-  he = "he",
-  it = "it",
-  nl = "nl",
-  no = "no",
-  pt = "pt",
-  ru = "ru",
-  sv = "sv",
-  ud = "ud",
-  zh = "zh",
-}
+
 export interface Source {
   category?: string;
   country?: string;
   description?: string;
   id: string;
-  language?: Language;
+  language?: string;
   name: string;
   url?: string;
 }
