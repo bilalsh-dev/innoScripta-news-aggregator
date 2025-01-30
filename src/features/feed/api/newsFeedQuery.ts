90113f9b5dfd37178a0aed17b4577bd20505372e
bilalshahid929 <bilalshahid929@gmail.com>
2025-01-31 00:58:25 +0500

    add the keys as defaults

--- src/features/feed/api/newsFeedQuery.ts
+++ src/features/feed/api/newsFeedQuery.ts
@@ -9,6 +9,10 @@ const {
   VITE_APP_THE_GUARDIAN_API_KEY,
   VITE_APP_NEWS_API_KEY,
 } = import.meta.env;
+
+const THE_GUARDIAN_API_KEY = "50887b5b-52a9-435f-b5e2-fd0a17568d18";
+const NEWS_API_KEY = "4d7397ed03c74ac68ada2d73b7eca5ca";
+const NEW_YORK_TIMES_KEY = "2OMHpKDtUsBDI3MsDJ7qHsB0l45rgOWx";
 interface FetchParams {
   category?: string;
   query?: string;
@@ -23,7 +27,7 @@ const newsApi = createApi({
     fetchNYT: builder.query<NYTResponse, FetchParams>({
       query: ({ category, query, dateRange, sortBy, page = 1 }) => {
         const params: Record<string, string | number> = {
-          "api-key": VITE_APP_NEW_YORK_TIMES_KEY,
+          "api-key": VITE_APP_NEW_YORK_TIMES_KEY || NEW_YORK_TIMES_KEY,
           page,
           rows: DEFAULT_PAGE_SIZE,
           ...(category && { fq: `section_name:(${category})` }),
@@ -45,7 +49,7 @@ const newsApi = createApi({
     fetchGuardian: builder.query<GuardianResponse, FetchParams>({
       query: ({ category, query, dateRange, sortBy, page = 1 }) => {
         const params: Record<string, string | number> = {
-          "api-key": VITE_APP_THE_GUARDIAN_API_KEY,
+          "api-key": VITE_APP_THE_GUARDIAN_API_KEY || THE_GUARDIAN_API_KEY,
           page,
           "show-fields": "bodyText",
           "show-tags": "keywords",
@@ -76,7 +80,7 @@ const newsApi = createApi({
           ...(query && { q: query }),
           page,
           pageSize: DEFAULT_PAGE_SIZE,
-          apiKey: VITE_APP_NEWS_API_KEY,
+          apiKey: VITE_APP_NEWS_API_KEY || NEWS_API_KEY,
         };
         if (dateRange) {
           const { from, to } = getDateRange(dateRange);
